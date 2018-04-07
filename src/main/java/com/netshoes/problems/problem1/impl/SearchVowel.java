0fe8c3fbd8fe790c8293438fc87deba3da15e97b
Libanori <senna@BR-N-H0MCSL2.NETSHOES.LOCAL>
2018-04-07 13:20:29 -0300

    Obrigado

--- src/main/java/com/netshoes/problems/problem1/impl/SearchVowel.java
+++ src/main/java/com/netshoes/problems/problem1/impl/SearchVowel.java
@@ -5,12 +5,143 @@ import com.netshoes.problems.problem1.api.Stream;
 // NAO RENOMEAR ESSA CLASSE!
 public class SearchVowel {
 
-    // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
+    
+	
+	public static char firstChar(Stream input) {
+        char test;
+        char result = '0';
+        boolean a = false;
+        boolean e = false;
+        boolean i = false;
+        boolean o = false;
+        boolean u = false;
+        int aut = 0;
+		
+        while(input.hasNext()) {
+        	test = input.getNext();
+        	
+        	switch (test) {
+			case 'a':
+				if(!a) {
+					a = true;
+					if(aut == 2)
+						result = 'a';
+				}
+				else
+					if(result == 'a')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'A':
+				if(!a) {
+					a = true;
+					if(aut == 2)
+						result = 'a';
+				}
+				else
+					if(result == 'a')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'e':
+				if(!e) {
+					e = true;
+					if(aut == 2)
+						result = 'e';
+				}
+				else
+					if(result == 'e')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'E':
+				if(!e) {
+					e = true;
+					if(aut == 2)
+						result = 'e';
+				}
+				else
+					if(result == 'e')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'i':
+				if(!i) {
+					i = true;
+					if(aut == 2)
+						result = 'i';
+				}
+				else
+					if(result == 'i')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'I':
+				if(!i) {
+					i = true;
+					if(aut == 2)
+						result = 'i';
+				}
+				else
+					if(result == 'i')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'o':
+				if(!o) {
+					o = true;
+					if(aut == 2)
+						result = 'o';
+				}
+				else
+					if(result == 'o')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'O':
+				if(!o) {
+					o = true;
+					if(aut == 2)
+						result = 'o';
+				}
+				else
+					if(result == 'o')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'u':
+				if(!u) {
+					u = true;
+					if(aut == 2)
+						result = 'u';
+				}
+				else
+					if(result == 'u')
+						result = '0';
+				aut = 1;				
+				break;
+			case 'U':
+				if(!u) {
+					u = true;
+					if(aut == 2)
+						result = 'u';
+				}
+				else
+					if(result == 'u')
+						result = '0';
+				aut = 1;				
+				break;
 
-    // NAO ALTERAR ESSA ASSINATURA!
-    public static char firstChar(Stream input) {
-        // TODO sua implementacao! Nesse ponto deve ser feita a adaptacoes necessarias para que os testes executem com sucesso
-        return 'x';
+			default:
+				if(aut == 1)
+					aut++;
+				else
+					aut = 0;
+				break;
+			}
+        }
+        
+		return result;
     }
 
 }
--- src/main/java/com/netshoes/problems/problem2/impl/CompareVersionImpl.java
+++ src/main/java/com/netshoes/problems/problem2/impl/CompareVersionImpl.java
@@ -5,7 +5,25 @@ public class CompareVersionImpl {
 
     // Nao alterar a assinatura desse metodo
     public static int compareVersion(String version1, String version2) {
-        // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
-        return 2;
+        String[] v1 = version1.split(".");
+        String[] v2 = version2.split(".");
+        
+        for(int i = 0; i < v1.length && i < v2.length; i++) {
+        	if (Integer.parseInt(v1[i]) > Integer.parseInt(v2[i]))
+        		return 1;
+        	if (Integer.parseInt(v1[i]) < Integer.parseInt(v2[i]))
+        		return -1;        	
+        }
+    	if(v1.length > v2.length) {
+    		for(int i = v2.length; i < v1.length; i++)
+    			if(Integer.parseInt(v1[i]) > 0)
+    				return 1;
+    	}
+        if(v1.length < v2.length) {
+        	for(int i = v1.length; i < v2.length; i++)
+        		if(Integer.parseInt(v2[i]) > 0)
+        			return -1;
+    	}
+        return 0;
     }
 }
